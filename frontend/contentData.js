0e7002619d39d515684dac6ff43033801e0008fd
Andre Silva <andrenations@gmail.com>
2025-01-09 23:38:55 -0500

    add content array with titles and paragraphs

---
+++ frontend/contentData.js
@@ -0,0 +1,17 @@
+export const content = [
+    {
+      title: "Take charge of your team with robust administrative tools",
+      paragraph:
+        "Take charge of your team with robust administrative tools that empower leaders to streamline operations and foster collaboration. These tools enable seamless task management, ensuring everyone stays aligned and productive.",
+    },
+    {
+      title: "Ensure the security of your data.",
+      paragraph:
+        "Ensure the security of your data with advanced protection measures designed to safeguard sensitive information. Robust encryption and secure access controls protect against unauthorized breaches.",
+    },
+    {
+      title: "Effortlessly restore deleted files with just a few clicks",
+      paragraph:
+        "Recover lost files instantly with a simple, user-friendly interface. Effortlessly restore deleted data in just a few clicks, saving time and stress. Enjoy peace of mind knowing your files are always retrievable.",
+    },
+  ];
