a1644937617ccd78153cc05763be07ad59a8d00e
Andre Silva <andrenations@gmail.com>
2025-01-09 23:39:58 -0500

    integrate Header and ScrollSection components in App layout

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import ScrollSection from "./components/ScrollSection";
+import Header from "./components/Header";
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <ScrollSection />;
+    </div>
+  );
+};
+
+export default App;
